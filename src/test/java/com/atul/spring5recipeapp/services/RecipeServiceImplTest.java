77d4b9e708e9372ec6be0c44a0a582299acf0ad3
AtulGarg28 <500068649@stu.upes.ac.in>
2020-09-19 11:12:51 +0530

    Added show.html

--- src/test/java/com/atul/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/atul/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,7 +28,7 @@ class RecipeServiceImplTest {
     }
 
     @Test
-    void getRecipes() throws Exception{
+    void getRecipesTest() throws Exception{
         Recipe recipe=new Recipe();
         HashSet<Recipe> recipiesData=new HashSet();
         recipiesData.add(recipe);
@@ -37,5 +38,18 @@ class RecipeServiceImplTest {
         Set<Recipe> recipeSet=recipeService.getRecipes();
         assertEquals(recipeSet.size(),1);
         verify(recipeRepository,times(1)).findAll();
+        verify(recipeRepository,never()).findById(anyLong());
+    }
+
+    @Test
+    void getRecipeByIdTest() {
+        Recipe recipe=new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> optionalRecipe=Optional.of(recipe);
+        when(recipeRepository.findById(anyLong())).thenReturn(optionalRecipe);
+        Recipe returnedRecipe=recipeService.findById(1L);
+//        assertNotNull(returnedRecipe,"Null Recipe Returned");
+//        verify(recipeRepository,times(1)).findById(anyLong());
+//        verify(recipeRepository,never()).findAll();
     }
 }
