d88973c220d87da485c9827485e0dcc05f46f97b
AtulGarg28 <500068649@stu.upes.ac.in>
2020-10-03 12:39:03 +0530

    For some updates in IngredientServiceImplTest but can't properly solve the errors. Closes #12.

--- src/test/java/com/atul/spring5recipeapp/services/IngredientServiceImplTest.java
+++ src/test/java/com/atul/spring5recipeapp/services/IngredientServiceImplTest.java
@@ -50,60 +50,60 @@ class IngredientServiceImplTest {
     public void findByRecipeIdAndId() throws Exception {
     }
 
-//    @Test
-//    void findByRecipeIdAndIngredientIdTest() throws Exception {
-//        //given
-//        Recipe recipe=new Recipe();
-//        recipe.setId(1L);
-//
-//        Ingredient ingredient1=new Ingredient();
-//        ingredient1.setId(1L);
-//
-//        Ingredient ingredient2=new Ingredient();
-//        ingredient2.setId(2L);
-//
-//        Ingredient ingredient3=new Ingredient();
-//        ingredient3.setId(3L);
-//
-//        recipe.addIngredient(ingredient1);
-//        recipe.addIngredient(ingredient2);
-//        recipe.addIngredient(ingredient3);
-//
-//        Optional<Recipe> recipeOptional=Optional.of(recipe);
-//
-//        //when
-//        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
-//
-//        IngredientCommands ingredientCommands=ingredientService.findByRecipeIdAndIngredientId(1L,3L);
-//
-//        //then
-//        assertEquals(Long.valueOf(3L),ingredientCommands.getId());
-//
-//        //ERROR.... NOT STORING THE RECIPE ID IN INGREDIENT.
-//        assertEquals(Long.valueOf(1L),ingredientCommands.getRecipeId());
-//        verify(recipeRepository,times(1)).findById(anyLong());
-//    }
+    @Test
+    void findByRecipeIdAndIngredientIdTest() throws Exception {
+        //given
+        Recipe recipe=new Recipe();
+        recipe.setId(1L);
 
-//    @Test
-//    public void saveIngredientCommandTest() throws Exception {
-//        IngredientCommands ingredientCommands=new IngredientCommands();
-//        ingredientCommands.setId(3L);
-//        ingredientCommands.setRecipeId(2L);
-//
-//        Optional<Recipe> optionalRecipe=Optional.of(new Recipe());
-//
-//        Recipe savedRecipe=new Recipe();
-//        savedRecipe.addIngredient(new Ingredient());
-//        savedRecipe.getIngredients().iterator().next().setId(3L);
-//
-//        when(recipeRepository.findById(anyLong())).thenReturn(optionalRecipe);
-//        when(recipeRepository.save(any())).thenReturn(savedRecipe);
-//
-//        IngredientCommands savedCommands=ingredientService.saveIngredientCommand(ingredientCommands);
-//
-//        assertEquals(Long.valueOf(3L),savedCommands.getId());
-//        verify(recipeRepository,times(1)).findById(anyLong());
-//        verify(recipeRepository,times(1)).save(any(Recipe.class));
-//
-//    }
+        Ingredient ingredient1=new Ingredient();
+        ingredient1.setId(1L);
+
+        Ingredient ingredient2=new Ingredient();
+        ingredient2.setId(2L);
+
+        Ingredient ingredient3=new Ingredient();
+        ingredient3.setId(3L);
+
+        recipe.addIngredient(ingredient1);
+        recipe.addIngredient(ingredient2);
+        recipe.addIngredient(ingredient3);
+
+        Optional<Recipe> recipeOptional=Optional.of(recipe);
+
+        //when
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        IngredientCommands ingredientCommands=ingredientService.findByRecipeIdAndIngredientId(1L,3L);
+
+        //then
+        assertEquals(Long.valueOf(3L),ingredientCommands.getId());
+
+        //ERROR.... NOT STORING THE RECIPE ID IN INGREDIENT.
+        assertEquals(Long.valueOf(1L),ingredientCommands.getRecipeId());
+        verify(recipeRepository,times(1)).findById(anyLong());
+    }
+
+    @Test
+    public void saveIngredientCommandTest() throws Exception {
+        IngredientCommands ingredientCommands=new IngredientCommands();
+        ingredientCommands.setId(3L);
+        ingredientCommands.setRecipeId(2L);
+
+        Optional<Recipe> optionalRecipe=Optional.of(new Recipe());
+
+        Recipe savedRecipe=new Recipe();
+        savedRecipe.addIngredient(new Ingredient());
+        savedRecipe.getIngredients().iterator().next().setId(3L);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(optionalRecipe);
+        when(recipeRepository.save(any())).thenReturn(savedRecipe);
+
+        IngredientCommands savedCommands=ingredientService.saveIngredientCommand(ingredientCommands);
+
+        assertEquals(Long.valueOf(3L),savedCommands.getId());
+        verify(recipeRepository,times(1)).findById(anyLong());
+        verify(recipeRepository,times(1)).save(any(Recipe.class));
+
+    }
 }
